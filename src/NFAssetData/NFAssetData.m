83e11f9840b11fc18c16affebf4dff83aa0b660d
cbcrouch <cayce82@yahoo.com>
2015-06-12 20:21:56 -0400

    Restructuring by moving files out of NFRenderer that shouldn't make GL calls
    NFAssetData will be for data that has been parsed into the framework
    but hasn’t been compressed and encoded into a runtime format.

--- src/NFAssetData/NFAssetData.m
+++ src/NFAssetData/NFAssetData.m
@@ -446,8 +446,10 @@
 
 
     //
-    // TODO: should only perform texture setup if the asset and shader both support it
+    // TODO: should only perform texture setup if the asset and shader both support it, need to focus
+    //       on removing/moving texture code out of the asset data
     //
+
     // get texture unifrom location
     self.textureUniform = glGetUniformLocation(programObj.hProgram, (const GLchar *)"texSampler\0");
     NSAssert(self.textureUniform != -1, @"Failed to get texture uniform location");
