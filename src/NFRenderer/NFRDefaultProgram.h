88e4d1467bb64f895d443b2692118b022c6cb432
cbcrouch <cayce82@yahoo.com>
2015-06-30 14:23:48 -0400

    Finished breaking out shader implementation classes
    Renamed NFRPhoneProgram to NFRDefaultProgram and moved it into the
    correct source files.

--- src/NFRenderer/NFRDefaultProgram.h
+++ src/NFRenderer/NFRDefaultProgram.h
@@ -7,6 +7,47 @@
 
 #import <Foundation/Foundation.h>
 
-@interface NFRDefaultProgram : NSObject
+// NOTE: because both gl.h and gl3.h are included will get symbols for deprecated GL functions
+//       and they should absolutely not be used
+#define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
+#import <OpenGL/gl3.h>
+
+
+#import "NFRProgramProtocol.h"
+
+
+@interface NFRDefaultProgram : NSObject <NFRProgram>
+
+typedef struct phongMaterialUniform_t {
+    GLint ambientLoc;
+    GLint diffuseLoc;
+    GLint specularLoc;
+    GLint shineLoc;
+} phongMaterialUniform_t;
+
+typedef struct phongLightUniform_t {
+    GLint ambientLoc;
+    GLint diffuseLoc;
+    GLint specularLoc;
+    GLint positionLoc;
+} phongLightUniform_t;
+
+@property (nonatomic, assign) GLint vertexAttribute;
+@property (nonatomic, assign) GLint normalAttribute;
+@property (nonatomic, assign) GLint texCoordAttribute;
+
+@property (nonatomic, assign) phongMaterialUniform_t materialUniforms;
+@property (nonatomic, assign) phongLightUniform_t lightUniforms;
+
+@property (nonatomic, assign) GLint modelMatrixLocation;
+@property (nonatomic, assign) GLint viewPositionLocation;
+@property (nonatomic, assign) GLuint lightSubroutine;
+@property (nonatomic, assign) GLuint phongSubroutine;
+
+@property (nonatomic, assign) GLuint hUBO;
+
+@property (nonatomic, readwrite, assign) GLuint hProgram;
+
+- (void) loadProgramInputPoints;
 
 @end
